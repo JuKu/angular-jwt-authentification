6d3ab353efa7695f12c2a22088529d3fec4addfa
Justin Kuenzel <kuenzel.justin@t-online.de>
2021-03-23 14:47:31 +0100

    Add some services

--- projects/jwt-auth-lib/src/lib/jwt-auth-lib.module.ts
+++ projects/jwt-auth-lib/src/lib/jwt-auth-lib.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { JwtAuthLibComponent } from './jwt-auth-lib.component';
+import {HttpClientModule} from '@angular/common/http';
 
 
 
 @NgModule({
   declarations: [JwtAuthLibComponent],
   imports: [
+    HttpClientModule
   ],
   exports: [JwtAuthLibComponent]
 })
